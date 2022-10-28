d5aa613d307417a8fe2742a3de1cbc728d795e35
Ephraim Becker <emb16@outlook.com>
2022-10-27 20:06:54 -0400

    Portal now works.

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,12 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public int index;
     public string levelName;
 
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(index);
+        SceneManager.LoadScene(levelName);
     }
 }
