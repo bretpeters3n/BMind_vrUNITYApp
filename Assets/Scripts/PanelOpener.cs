700d804070c96ae2eeec2f9d316921ba0deeb8cc
carlos-UX-codes <102880003+carlos-UX-codes@users.noreply.github.com>
2022-11-13 15:55:14 -0600

    project back in Unity..
    project back in Unity..

--- Assets/Scripts/PanelOpener.cs
+++ Assets/Scripts/PanelOpener.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PanelOpener : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject Panel;
+    public void OpenPanel()
     {
-        
+        if (Panel != null)
+        {
+            Panel.SetActive(true);
+        }
+
+
     }
+    
+    
 }
---
+++ Assets/Team-folders/Bret-assets/Materials/Smoke FX Pack/Documentation/Readme.txt
@@ -0,0 +1,12 @@
+Thankyou for purchasing the Smoke FX Pack!
+This package contains five incredible smoke related effects to use in your gameprojects.
+
+* Big Smoke,
+* Green Flare Smoke FX, 
+* Red Flare Smoke FX, 
+* Regular Smoke FX,
+* Soft Smoke.
+
+
+
+
