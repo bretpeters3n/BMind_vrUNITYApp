5cfac3758cffd4259f96da3e2660ff06309c2cea
Bret Petersen <bretpetersen82@gmail.com>
2022-11-12 20:59:49 -0600

    Intro button interactions working

--- Assets/Team-folders/Bret-assets/scripts/ChangeSceneBret.cs
+++ Assets/Team-folders/Bret-assets/scripts/ChangeSceneBret.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour
+public class ChangeSceneBret : MonoBehaviour
 {
     public void MoveToScene(int sceneID)
     {
