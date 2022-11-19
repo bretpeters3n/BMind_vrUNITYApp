9f7c022443e3fd6cc7db3a680c9489b221ee5a8e
Bret Petersen <bretpetersen82@gmail.com>
2022-11-19 15:30:37 -0600

    Imported Amany scene 2 package and organized assets

---
+++ Assets/Team-folders/Amany-assets/Script/StarScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarScript : MonoBehaviour
+{
+    public GameObject StarScriptPrefab;
+
+    public void CreateStarScript()
+    {
+        GameObject newStar = Instantiate(StarScriptPrefab);   
+    }
+}
